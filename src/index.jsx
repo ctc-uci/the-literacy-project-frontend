72f99b8a9cf5134b4306af44125685399cbe751b
Alan Chang <alan@alanchang.xyz>
2021-11-20 17:04:06 -0800

    Setup React Router and add first route in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
+// page imports
+import StudentView from './pages/students/students';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<App />} />
+        <Route path="/students" exact element={<StudentView />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
---
+++ src/pages/students/students.jsx
@@ -0,0 +1,12 @@
+import './students.css';
+import React from 'react';
+
+const StudentView = () => {
+  return (
+    <div>
+      <h1 className="student-view">Student View</h1>
+    </div>
+  );
+};
+
+export default StudentView;
