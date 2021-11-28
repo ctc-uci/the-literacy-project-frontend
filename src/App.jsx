1848e07ccdc4e1c5a4cc8fa7de3fec9ddd5f79c3
michellelin1 <michellelin810@gmail.com>
2021-11-27 21:23:06 -0800

    created form

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,13 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import AttitudeForm from './components/AttitudeForm/AttitudeForm';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div className="mb-2">
-          <Button
-            variant="primary"
-            href="https://react-bootstrap.github.io/getting-started/why-react-bootstrap/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn more about react-bootstrap here!
-          </Button>{' '}
-        </div>
-        <p>This is a React App!</p>
-      </header>
+      <AttitudeForm />
     </div>
   );
 }
--- src/components/AttitudeForm/AttitudeForm.jsx
+++ src/components/AttitudeForm/AttitudeForm.jsx
@@ -1,12 +1,28 @@
-// import React, { useState } from 'react';
+import React from 'react';
 import './AttitudeForm.css';
 
 function AttitudeForm() {
-  // return(
-  //   <div>
-  //     {/* TODO:  */}
-  //   </div>
-  // );
+  return (
+    <div>
+      <h2>Elementary Reading Attitude Survey Scoring Sheet</h2>
+      <form>
+        <label htmlFor="studentName">
+          Student Name:
+          <input type="text" id="studentName" />
+        </label>
+        <label htmlFor="masterTeacher">
+          Master Teacher:
+          <input type="text" id="masterTeacher" />
+        </label>
+        <label htmlFor="grade">
+          Grade:
+          <input type="text" id="grade" />
+        </label>
+
+        {/* {call AttitudeScoreTable component} */}
+      </form>
+    </div>
+  );
 }
 
 export default AttitudeForm;
