1eceafea12a60e5c6aafede2654c499e52ec2724
rchampio <rchampio@uci.edu>
2021-11-27 16:22:41 -0800

    Added AttitudeForm/ScoreTable/TableRow Components

---
+++ src/components/AttitudeForm/AttitudeForm.jsx
@@ -0,0 +1,12 @@
+// import React, { useState } from 'react';
+import './AttitudeForm.css';
+
+function AttitudeForm() {
+  // return(
+  //   <div>
+  //     {/* TODO:  */}
+  //   </div>
+  // );
+}
+
+export default AttitudeForm;
