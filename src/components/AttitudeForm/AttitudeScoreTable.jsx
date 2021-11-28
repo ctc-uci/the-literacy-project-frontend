4eae319cb5819380a902556a76501ce44373aa8a
rchampio <rchampio@uci.edu>
2021-11-27 16:11:28 -0800

    Added AttitudeForm/ScoreTable/TableRow Components

---
+++ src/components/AttitudeForm/AttitudeScoreTable.jsx
@@ -0,0 +1,17 @@
+// import React, { useState } from 'react';
+import './AttitudeForm.css';
+
+function AttitudeScoreTable() {
+  // return(
+  //   <div>
+  //     {/* TODO: Create a Title (ie Post Test Date) */}
+  //     {/* TODO: Form/Table here --> two headers: (Recreational Reading, Academic Reading)
+  //                   --> Rows (input drop down for each row? / component)
+  //                   --> Totals PER Column
+  //                   --> Overall Total of both Recreational Reading + Academic Reading scores.
+  //     */}
+  //   </div>
+  // );
+}
+
+export default AttitudeScoreTable;
---
+++ src/components/AttitudeForm/AttitudeScoreTableRow.jsx
@@ -0,0 +1,13 @@
+// import React, { useState } from 'react';
+import './AttitudeForm.css';
+
+function AttitudeScoreTable() {
+  // return(
+  //   <div>
+  //     {/* TODO: Row for Rec Reading + Academic Reading */}
+  //     {}
+  //   </div>
+  // );
+}
+
+export default AttitudeScoreTable;
