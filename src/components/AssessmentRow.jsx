08d617fafefebef60e71b7cf7015f0fc206608ed
mattcalwu <mattcalwu@gmail.com>
2021-11-27 23:45:10 -0800

    Updated percentage calculations

--- src/components/AssessmentRow.jsx
+++ src/components/AssessmentRow.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-function AssessmentRow({ testNumber, gameName, skillTest, passingScore }) {
+function AssessmentRow({ testNumber, gameName, skillTest, passingScore, numQuestions }) {
   const [preScore, setPreScore] = useState(0);
   const [postScore, setPostScore] = useState(0);
 
@@ -13,23 +13,40 @@ function AssessmentRow({ testNumber, gameName, skillTest, passingScore }) {
     setPostScore(event.target.value);
   };
 
+  const change = (postScore - preScore) / preScore;
+  if (numQuestions !== 0) {
+    return (
+      <tr>
+        <td>{testNumber}</td>
+        <td>{gameName}</td>
+        <td>{skillTest}</td>
+        <td>{passingScore}</td>
+        <td>
+          <form onChange={handlePreScore}>
+            <input placeholder={0} type="number" name="Pre Score" min={0} max={numQuestions} />/
+            {numQuestions}
+          </form>
+        </td>
+        <td>
+          <form onChange={handlePostScore}>
+            <input placeholder={0} type="number" name="Post Score" min={0} max={numQuestions} />/
+            {numQuestions}
+          </form>
+        </td>
+        <td>{preScore / numQuestions === 0 ? '---' : change * 100}%</td>
+      </tr>
+    );
+  }
+
   return (
     <tr>
       <td>{testNumber}</td>
       <td>{gameName}</td>
       <td>{skillTest}</td>
-      <td>{passingScore}</td>
-      <td>
-        <form onChange={handlePreScore}>
-          <input type="text" name="Pre Score" />
-        </form>
-      </td>
-      <td>
-        <form onChange={handlePostScore}>
-          <input type="text" name="Post Score" />
-        </form>
-      </td>
-      <td>{postScore - preScore}</td>
+      <td>---</td>
+      <td>-----</td>
+      <td>-----</td>
+      <td>---</td>
     </tr>
   );
   // Notes for later:
@@ -41,6 +58,7 @@ AssessmentRow.defaultProps = {
   gameName: '',
   skillTest: 'test',
   passingScore: '1/3',
+  numQuestions: 0,
 };
 
 AssessmentRow.propTypes = {
@@ -48,6 +66,7 @@ AssessmentRow.propTypes = {
   gameName: PropTypes.string,
   skillTest: PropTypes.string,
   passingScore: PropTypes.string,
+  numQuestions: PropTypes.number,
 };
 
 export default AssessmentRow;
--- src/components/AssessmentScoreCard.jsx
+++ src/components/AssessmentScoreCard.jsx
@@ -55,78 +55,91 @@ function AssessmentScoreCard() {
           gameName="Alphabet Warm-Up Letters"
           skillTest="m, p, t, e, k, c, D, b, i, L, u, w, a, S, z"
           passingScore="13/15"
+          numQuestions={15}
         />
         <AssessmentRow
           testNumber={2}
           gameName="Alphabet Warm-Up Sounds"
           skillTest="/m/, /d/, /p/, /f/, /w/, /l/, /h/"
           passingScore="6/7"
+          numQuestions={7}
         />
         <AssessmentRow
           testNumber={3}
           gameName="Short Vowel Kick"
           skillTest="pat, hit, bet, hut, hop, nak, rit, lep, tum, rof"
           passingScore="8/10"
+          numQuestions={10}
         />
         <AssessmentRow
           testNumber={4}
           gameName="Slight Word Drop Kick"
           skillTest="is, of, two, are, the, you, does, give, said, some, want, were, their, where, would"
           passingScore="10/15"
+          numQuestions={15}
         />
         <AssessmentRow
           testNumber={5}
           gameName="Consonant Digraph Corner Kick"
           skillTest="shoe, church, photo, song, what"
           passingScore="5/5"
+          numQuestions={5}
         />
         <AssessmentRow
           testNumber={6}
           gameName="Long & Short Vowel Pass"
           skillTest="no assessment"
           passingScore="n/a"
+          numQuestions={0}
         />
         <AssessmentRow
           testNumber={7}
           gameName="Consonant Blend Bicycle Kick"
           skillTest="flag, skip, truck, snap, glad, spin, club"
           passingScore="5/7"
+          numQuestions={7}
         />
         <AssessmentRow
           testNumber={8}
           gameName="Consonant Blend & Digraph Dribble"
           skillTest="no assessment"
           passingScore="n/a"
+          numQuestions={0}
         />
         <AssessmentRow
           testNumber={9}
           gameName="Long Vowel Throw In"
           skillTest="street, pain, beat, foe, clay, boat"
           passingScore="4/6"
+          numQuestions={6}
         />
         <AssessmentRow
           testNumber={10}
           gameName='"C" and "G" Slide Tackle'
           skillTest="city, cat, go, gem, cep, cote, gak, gip"
           passingScore="4/8"
+          numQuestions={8}
         />
         <AssessmentRow
           testNumber={11}
           gameName="Affix Header"
           skillTest="slipped, mailed, wanted, thinking, coats, slows"
           passingScore="4/6"
+          numQuestions={6}
         />
         <AssessmentRow
           testNumber={12}
           gameName="Diphthong Diving Save"
           skillTest="cloud, plow, soil, grew, blue, haul, paw, look, cook"
           passingScore="5/9"
+          numQuestions={9}
         />
         <AssessmentRow
           testNumber={13}
           gameName="R-controlled Penalty Kick"
           skillTest="cork, fir, car, term, fur"
           passingScore="4/5"
+          numQuestions={5}
         />
         <label htmlFor="notes">
           Notes:
