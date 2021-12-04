ec331be8fd53d4eb0139e0bf7503e5ea2b689063
Alan Chang <alan@alanchang.xyz>
2021-12-04 03:25:55 -0800

    Add School and Student-related routes

---
+++ src/pages/assign-students/assign-students.jsx
@@ -0,0 +1,12 @@
+import './assign-students.css';
+import React from 'react';
+
+const AssignStudentsView = () => {
+  return (
+    <div>
+      <h1 className="assign-students-view">Assign Students View</h1>
+    </div>
+  );
+};
+
+export default AssignStudentsView;
---
+++ src/pages/schools-create/schools-create.jsx
@@ -0,0 +1,12 @@
+import './schools-create.css';
+import React from 'react';
+
+const SchoolsCreateView = () => {
+  return (
+    <div>
+      <h1 className="schools-create-view">Schools Create View</h1>
+    </div>
+  );
+};
+
+export default SchoolsCreateView;
