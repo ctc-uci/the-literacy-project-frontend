883d8afbae9867b22c9d7d78592df46e27d5dce2
Lee <lbahir@uci.edu>
2022-01-26 16:29:26 -0800

    added user tabs, remember checkbox, grid columns, to login screen

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -10,59 +10,84 @@ function LoginForm() {
   };
 
   // checkbox under inputs, toggles remembering user
-  const [rememberMe, setRememberMe] = React.useState(false);
+  const [rememberMe, setRememberMe] = useState(false);
   const toggleRememberMe = () => {
     setRememberMe(!rememberMe);
   };
 
+  // tabs above form, sets user to admin/teacher and adjusts background
+  const [loginColor, setLoginColor] = useState('#1A4767');
+  // const [userType, setUserType] = useState('Admin');
+  const setUserAdmin = () => {
+    // setUserType('Admin');
+    setLoginColor('#1A4767');
+  };
+  const setUserTeacher = () => {
+    // setUserType('Teacher');
+    setLoginColor('#6A91BC');
+  };
+
   return (
-    <div className="loginFormWrapper">
-      <h1> Log In </h1>
+    <div>
+      {/* Admin/Teacher tabs above form */}
+      <div className="userTabs">
+        <button id="adminButton" type="button" onClick={setUserAdmin}>
+          Admin
+        </button>
+        <button id="teacherButton" type="button" onClick={setUserTeacher}>
+          Teacher
+        </button>
+      </div>
 
-      <form>
-        <div className="emailInput">
-          <label htmlFor="email">
-            Email
-            <br />
-            <input type="text" id="email" placeholder="Email Address" />
-          </label>
+      {/* body of LoginForm */}
+      <div className="formWrapper" style={{ backgroundColor: loginColor }}>
+        <h1 className="title"> Log In </h1>
+
+        <form>
+          <div className="emailInput">
+            <label htmlFor="email">
+              Email
+              <br />
+              <input type="text" id="email" placeholder="Email Address" />
+            </label>
+          </div>
+
+          <div className="passwordInput">
+            <label htmlFor="password">
+              Password
+              <br />
+              <input
+                type={passwordShown ? 'text' : 'password'}
+                id="password"
+                placeholder="Password"
+              />
+              <FaEye id="eyeIcon" color="black" onClick={togglePasswordVisibility} />
+            </label>
+          </div>
+
+          <div className="rememberMeBox">
+            <label htmlFor="rememberBox">
+              <input
+                type="checkbox"
+                id="rememberBox"
+                checked={rememberMe}
+                onChange={toggleRememberMe}
+              />
+              Remember Me?
+            </label>
+          </div>
+
+          <input id="submitButton" type="submit" value="Login" />
+        </form>
+
+        <div className="links">
+          <a id="forgotPassword" href="/">
+            Forgot password?
+          </a>
+          <a id="noAccount" href="/">
+            Don&apos;t have an account?
+          </a>
         </div>
-
-        <div className="passwordInput">
-          <label htmlFor="password">
-            Password
-            <br />
-            <input
-              type={passwordShown ? 'text' : 'password'}
-              id="password"
-              placeholder="Password"
-            />
-            <FaEye id="eyeIcon" color="black" onClick={togglePasswordVisibility} />
-          </label>
-        </div>
-
-        <div className="rememberMeBox">
-          <label htmlFor="rememberBox">
-            <input
-              type="checkbox"
-              id="rememberBox"
-              checked={rememberMe}
-              onChange={toggleRememberMe}
-            />
-            Remember Me?
-          </label>
-        </div>
-
-        <input id="submitButton" type="submit" value="Login" />
-      </form>
-
-      <div className="links">
-        <a id="forgotPassword" href="/">
-          Forgot password?
-        </a>
-        <a id="noAccount" href="/">
-          Don&apos;t have an account?
-        </a>
       </div>
     </div>
   );
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,9 +1,39 @@
 import './login.css';
 import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
+
 import LoginForm from '../../components/LoginForm/LoginForm';
 
 const LoginView = () => {
-  return <LoginForm />;
+  return (
+    <div>
+      {/* fluid container fills entire width and height */}
+      <Container fluid className="vh-100">
+        {/* row contains 12 columns, fills entire height */}
+        <Row
+          className="h-100"
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          {/* 8 Columns for left side with LoginForm */}
+          <Col sm={{ span: 4, offset: 2 }}>
+            <LoginForm />
+          </Col>
+          <Col sm={2}> </Col>
+
+          {/* 4 Columns for right side with logo and images */}
+          <Col sm={4} className="logoSide">
+            LOGO AND IMAGES GO HERE
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default LoginView;
