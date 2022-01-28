df166ad7541bac9b4f2f48d2bae7e9641b2f9807
Ruslan Manoharan <rmanohar@uci.edu>
2022-01-27 20:39:30 -0800

    Created navigation bar, still need to properly route the links using href.

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,61 +1,36 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import Image from './tlp.png';
 
 function NavigationBar() {
   return (
-    <html lang="en">
-      <head>
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl"
-          crossOrigin="anonymous"
-        />
-      </head>
-      <body>
-        <div className="container">
-          <nav className="navbar navbar-expand-sm navbar-light ">
-            <a className="navbar-brand" href="/#">
-              <img src="tlp.png" width="170" height="127.5" alt="" />
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#toggleMobileMenu"
-              aria-controls="toggleMobileMenu"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon" />
-            </button>
-            <div className="collapse navbar-collapse" id="toggleMobileMenu">
-              <ul className="navbar-nav mr-auto">
-                <li>
-                  <a className="nav-link" href="/dashboard">
-                    Home
-                  </a>
-                </li>
-                <li>
-                  <a className="nav-link" href="/dashboard">
-                    Area Management
-                  </a>
-                </li>
-                <li>
-                  <a className="nav-link" href="/settings">
-                    Settings
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </nav>
-        </div>
-        <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0"
-          crossOrigin="anonymous"
-        />
-      </body>
-    </html>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="/dashboard">
+          <img src={Image} width="100" height="75" className="d-inline-block" alt="" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/dashboard" style={{ color: '#6A91BC' }}>
+              Home
+            </Nav.Link>
+            <Nav.Link href="/dashboard" style={{ color: '#6A91BC' }}>
+              Link
+            </Nav.Link>
+            <Nav.Link href="/settings" style={{ color: '#6A91BC' }}>
+              Settings
+            </Nav.Link>
+          </Nav>
+          <Nav className="mr-auto">
+            <Nav.Link href="/login" style={{ color: '#E53E3E' }}>
+              Log Out
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
