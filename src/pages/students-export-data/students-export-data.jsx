b6347ec29f76bde832ef7f4901aeac51c1f73e96
Anikait Rao <anikaitrules@gmail.com>
2022-02-16 17:10:38 -0800

    formatting

--- src/pages/students-export-data/students-export-data.jsx
+++ src/pages/students-export-data/students-export-data.jsx
@@ -1,17 +1,10 @@
 import './students-export-data.css';
 import React from 'react';
 
-import AttitudeForm from '../../components/AttitudeForm/AttitudeForm';
-
 const StudentsExportDataView = () => {
   return (
     <div>
       <h1 className="students-export-data-view">Students Export Data View</h1>
-
-      {/*  TEMP CONTAINER TO SHOW ATTITUDE FORM */}
-      <div style={{ width: '510px' }}>
-        <AttitudeForm />
-      </div>
     </div>
   );
 };
