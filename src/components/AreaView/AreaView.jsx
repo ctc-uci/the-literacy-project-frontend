057f288135e94db94950a96fe3c1941864c90286
Ruslan Manoharan <rmanohar@uci.edu>
2022-03-08 21:44:17 -0800

    Formatted the area-details page. Small adjustment needed on dropdown menu component, everything else looks fine.

--- src/components/AreaView/AreaView.jsx
+++ src/components/AreaView/AreaView.jsx
@@ -1,9 +1,11 @@
 import { React, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Button, Card } from 'react-bootstrap';
 import DropdownMenu from '../../common/DropdownMenu/DropdownMenu';
 import SitesTable from './sitesTable';
-import './AreaView.module.css';
+import styles from './AreaView.module.css';
 import Plus from '../../assets/icons/plus.svg';
+import NavigationBarTwo from '../NavigationBarTwo/NavigationBarTwo';
 
 // const AreaView = (areaName) => {
 const AreaView = () => {
@@ -19,42 +21,63 @@ const AreaView = () => {
 
   return (
     <div>
-      <h1>
-        <Link to="/area-management" className="all-areas-breadcrumb">
-          Areas{' '}
-        </Link>
-        / {areaName}
-      </h1>
-      <div className="search-query-filters">
-        <div className="select-school-year">
-          School Year
-          <DropdownMenu choices={schoolYearChoices} current={schoolYear} setFn={setSchoolYear} />
-        </div>
-        <div className="select-school-year">
-          Cycle
-          <DropdownMenu choices={cycleChoices} current={cycle} setFn={setCycle} />
-        </div>
-        <h2> Cycle: {cycle} </h2>
-        <div className="site-search-bar">
-          <input
-            type="text"
-            placeholder="Search sites"
-            defaultValue={searchQuery}
-            onChange={e => setSearchQuery(e.target.value)}
-          />
-          <div> Search </div>
-        </div>
+      <NavigationBarTwo />
+      <div className={styles.site_container}>
+        <div>
+          <h1>
+            <Link to="/area-management" className={styles.all_areas_breadcrumb}>
+              Areas{' '}
+            </Link>
+            / {areaName}
+          </h1>
+          <div className={styles.search_query_filters}>
+            <h1>School Year</h1>
+            <div className={styles.select_school_year}>
+              <DropdownMenu
+                choices={schoolYearChoices}
+                current={schoolYear}
+                setFn={setSchoolYear}
+              />
+            </div>
+            <h2>Cycle</h2>
+            <div className={styles.select_school_year}>
+              <DropdownMenu choices={cycleChoices} current={cycle} setFn={setCycle} />
+            </div>
+            <div className="site-search-bar">
+              <input
+                type="text"
+                placeholder="Search Sites"
+                defaultValue={searchQuery}
+                onChange={e => setSearchQuery(e.target.value)}
+              />
+              <Button variant="primary">Search</Button>
+            </div>
+          </div>
+          <div>
+            <Link to="/sites/create">
+              <button type="button" className="btn btn-warning">
+                Create New Site
+                <img className="plus__icon" src={Plus} alt="Plus Icon" />
+              </button>
+            </Link>
+          </div>
 
-        <Link to="/sites/create">
-          <button type="button" className="btn btn-warning">
-            Create New Site
-            <img className="plus__icon" src={Plus} alt="Plus Icon" />
-          </button>
-        </Link>
-      </div>
-
-      <div className="sites-table-container">
-        <SitesTable />
+          <div className="sites-table-container">
+            <SitesTable />
+          </div>
+        </div>
+        <div className="data">
+          <Button variant="primary">Export to CSV</Button>
+          <h2>Data</h2>
+          <h3>Average Scores</h3>
+          {/* placeholder for graph */}
+          <Card className="graph" />
+          <Card className="stats">
+            <p># Students</p>
+            <p># Teachers</p>
+            <p># Sites</p>
+          </Card>
+        </div>
       </div>
     </div>
   );
