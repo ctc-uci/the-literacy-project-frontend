a1042307a3bbf3f670f1f67e9a816fcae7d7d52a
Anikait Rao <anikaitrules@gmail.com>
2022-04-06 11:51:37 -0700

    todo for student profile box

--- src/components/StudentProfileBox/StudentProfileBox.jsx
+++ src/components/StudentProfileBox/StudentProfileBox.jsx
@@ -5,6 +5,7 @@ import { Button, Col, Row } from 'react-bootstrap';
 import styles from './StudentProfileBox.module.css';
 
 const StudentProfileBox = ({ studentName }) => {
+  // TODO: Add onclick for button to navigate to the student's profile page
   return (
     <Row className={styles['student-profile-row']}>
       <Col className="d-flex justify-content-center align-items-center">{studentName}</Col>
