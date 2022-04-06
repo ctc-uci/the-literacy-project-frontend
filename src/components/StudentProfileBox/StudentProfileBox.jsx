2c6fa95c00bc289c80f6503a03bc585fe0f86cfb
Crystal Lee <crystalleesf@gmail.com>
2022-04-06 10:12:51 -0700

    Add row to student profile box component

--- src/components/StudentProfileBox/StudentProfileBox.jsx
+++ src/components/StudentProfileBox/StudentProfileBox.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../../common/vars.css';
-import { Button, Col } from 'react-bootstrap';
+import { Button, Col, Row } from 'react-bootstrap';
 import styles from './StudentProfileBox.module.css';
 
 const StudentProfileBox = ({ studentName }) => {
   return (
-    <>
+    <Row className={styles['student-profile-row']}>
       <Col className="d-flex justify-content-center align-items-center">{studentName}</Col>
       <Col className="d-flex justify-content-center align-items-center">
         <Button className={styles['view-profile-btn']}>View Profile</Button>
       </Col>
-    </>
+    </Row>
   );
 };
 
