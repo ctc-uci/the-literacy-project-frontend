46c115ae82f135e4ac20f716ee906fe6ac43f178
Hang Cao <hmcao1@uci.edu>
2022-04-09 12:27:51 -0700

    empty view + padding/margin

--- src/pages/master-teacher/master-teacher.jsx
+++ src/pages/master-teacher/master-teacher.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import './master-teacher.module.css';
 import { instanceOf } from 'prop-types';
+import styles from './master-teacher.module.css';
 import NavigationBar from '../../components/NavigationBar/NavigationBar';
 import { withCookies, cookieKeys, Cookies } from '../../common/auth/cookie_utils';
 import { TLPBackend } from '../../common/utils';
 import StudentGroup from '../../components/StudentGroup/StudentGroup';
 import StudentProfileBox from '../../components/StudentProfileBox/StudentProfileBox';
+// import arrow from './arrow.png';
 
 const MasterTeacherView = ({ cookies }) => {
   const [selectedSiteName, setSelectedSiteName] = useState();
@@ -43,39 +44,55 @@ const MasterTeacherView = ({ cookies }) => {
     <div>
       <NavigationBar />
       <div>{/* toggle bar */}</div>
-      <div>
-        <h3>{selectedSiteName}</h3>
-        <h3>{siteAddress}</h3>
-      </div>
-      <div>
-        <h3>Data</h3>
-        {/* add the graphs */}
-      </div>
-      <div>
-        <h3>Student Groups</h3>
-        {studentGroups.map(group => (
-          <StudentGroup
-            key={group.groupId}
-            studentList={siteStudents
-              .map(s => {
-                return group.groupId === s.studentGroupId ? s.firstName : '';
-              })
-              .filter(t => {
-                return t !== '';
-              })}
-            meetingDay={group.meetingDay}
-            meetingTime={group.meetingTime}
-          />
-        ))}
-        {/* add the graphs */}
-      </div>
-      <div>
-        <h3>Students</h3>
-        {/* ?:Maybe alphabetical here? */}
-        {siteStudents.map(s => (
-          <StudentProfileBox key={s.studentId} studentName={`${s.firstName} ${s.lastName}`} />
-        ))}
-        {/* add the graphs */}
+      <div className={styles.main}>
+        <div className={styles.section}>
+          <h3>{selectedSiteName}</h3>
+          <h3 className={styles['gray-text']}>{siteAddress}</h3>
+        </div>
+        <div className={styles.section}>
+          <h3>Data</h3>
+          {/* add the graphs */}
+        </div>
+        <div className={styles.section}>
+          <h3>Student Groups</h3>
+          {studentGroups.length === 0 && (
+            <div className={styles['empty-view']}>
+              <h2>No Student Groups is available for this site yet.</h2>
+            </div>
+          )}
+          {studentGroups.map(group => (
+            <StudentGroup
+              key={group.groupId}
+              studentList={siteStudents
+                .map(s => {
+                  return group.groupId === s.studentGroupId ? s.firstName : '';
+                })
+                .filter(t => {
+                  return t !== '';
+                })}
+              meetingDay={group.meetingDay}
+              meetingTime={group.meetingTime}
+            />
+          ))}
+        </div>
+        <div className={styles.section}>
+          <h3>Students</h3>
+          {siteStudents.length === 0 && (
+            <div className={styles['empty-view']}>
+              <h2>No students have been created for this site yet.</h2>
+              <h2>Click here to create.</h2>
+              {/* <img
+                className={styles.arrow}
+                src={arrow}
+                alt="arrow pointing to create student button"
+              /> */}
+            </div>
+          )}
+          {/* ?:Maybe alphabetical here? */}
+          {siteStudents.map(s => (
+            <StudentProfileBox key={s.studentId} studentName={`${s.firstName} ${s.lastName}`} />
+          ))}
+        </div>
       </div>
     </div>
   );
