11bc9f0b0e3f6c2e821966f2b422f6e7954cfdbf
Sravan Tallapaka <sravtall@gmail.com>
2022-04-09 22:12:06 -0700

    updated styling for student groups and students sections

--- src/pages/master-teacher/master-teacher.jsx
+++ src/pages/master-teacher/master-teacher.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import './master-teacher.module.css';
 import { instanceOf } from 'prop-types';
-import NavigationBar from '../../components/NavigationBar/NavigationBar';
+import { Button } from 'react-bootstrap';
 import { withCookies, cookieKeys, Cookies } from '../../common/auth/cookie_utils';
 import { TLPBackend } from '../../common/utils';
+import Plus from '../../assets/icons/plus.svg';
+import NavigationBar from '../../components/NavigationBar/NavigationBar';
 import StudentGroup from '../../components/StudentGroup/StudentGroup';
 import StudentProfileBox from '../../components/StudentProfileBox/StudentProfileBox';
+import '../../common/vars.css';
+import styles from './master-teacher.module.css';
 
 const MasterTeacherView = ({ cookies }) => {
   const [selectedSiteName, setSelectedSiteName] = useState();
@@ -52,30 +55,48 @@ const MasterTeacherView = ({ cookies }) => {
         {/* add the graphs */}
       </div>
       <div>
-        <h3>Student Groups</h3>
-        {studentGroups.map(group => (
-          <StudentGroup
-            key={group.groupId}
-            studentList={siteStudents
-              .map(s => {
-                return group.groupId === s.studentGroupId ? s.firstName : '';
-              })
-              .filter(t => {
-                return t !== '';
-              })}
-            meetingDay={group.meetingDay}
-            meetingTime={group.meetingTime}
-          />
-        ))}
-        {/* add the graphs */}
+        <div className={styles['student-groups-header']}>
+          <h3 className={styles['mtd-subheadings']}>Student Groups</h3>
+          <Button variant="warning" className={styles['create-button']}>
+            Create Student Group
+            <img className={styles.plus__icon} src={Plus} alt="Plus Icon" />
+          </Button>
+        </div>
+        <div className={styles['student-groups-content']}>
+          {studentGroups.map(group => (
+            <StudentGroup
+              key={group.groupId}
+              studentList={siteStudents
+                .sort((a, b) => {
+                  if (a.firstName.toLowerCase() < b.firstName.toLowerCase()) return -1;
+                  if (a.firstName.toLowerCase() > b.firstName.toLowerCase()) return 1;
+                  return 0;
+                })
+                .map(s => {
+                  return group.groupId === s.studentGroupId ? s.firstName : '';
+                })
+                .filter(t => {
+                  return t !== '';
+                })}
+              meetingDay={group.meetingDay}
+              meetingTime={group.meetingTime}
+            />
+          ))}
+        </div>
       </div>
       <div>
-        <h3>Students</h3>
-        {/* ?:Maybe alphabetical here? */}
-        {siteStudents.map(s => (
-          <StudentProfileBox key={s.studentId} studentName={`${s.firstName} ${s.lastName}`} />
-        ))}
-        {/* add the graphs */}
+        <div className={styles['students-header']}>
+          <h3>Students</h3>
+          <Button variant="warning" className={styles['create-button']}>
+            Create New Student
+            <img className={styles.plus__icon} src={Plus} alt="Plus Icon" />
+          </Button>
+        </div>
+        <div className={styles['students-content']}>
+          {siteStudents.slice(0, 6).map(s => (
+            <StudentProfileBox key={s.studentId} studentName={`${s.firstName} ${s.lastName}`} />
+          ))}
+        </div>
       </div>
     </div>
   );
