b39f2a45488e25fb4bf772934a0210e734f04139
michellelin1 <michellelin810@gmail.com>
2022-04-12 20:25:58 -0700

    created access denied page

--- src/pages/access-denied/access-denied.jsx
+++ src/pages/access-denied/access-denied.jsx
@@ -1,10 +1,23 @@
-import './access-denied.module.css';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './access-denied.module.css';
+import error from '../../assets/404.PNG';
+import childGlass from '../../assets/personGlasses.PNG';
 
 const AccessDeniedView = () => {
   return (
     <div>
-      <h1 className="access-denied-view">You do not have access to this page</h1>
+      <img src={error} alt="404 warning" className={styles.error} />
+
+      <div className={styles.content}>
+        <h1 className={styles.text}>Whoops! We can&apos;t find the page your looking for...</h1>
+        <Link to="/">
+          <button type="button" className="btn btn-warning">
+            Go Back Home
+          </button>
+        </Link>
+      </div>
+      <img src={childGlass} className={styles.child} alt="child with glasses" />
     </div>
   );
 };
