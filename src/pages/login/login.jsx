c296d1906d532e037a76dc317540319d77d338e7
Anikait Rao <anikaitrules@gmail.com>
2022-04-12 21:21:04 -0700

    login.css to module css

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,4 +1,4 @@
-import './login.css';
+import './login.module.css';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
