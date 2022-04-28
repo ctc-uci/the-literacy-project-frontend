384b619d58d77758c460c32cd44303ca18a4e1f8
michellelin1 <michellelin810@gmail.com>
2022-04-27 23:12:26 -0700

    form validation for create student modal

--- src/components/CreateStudentModal/CreateStudentModal.jsx
+++ src/components/CreateStudentModal/CreateStudentModal.jsx
@@ -1,6 +1,6 @@
 import { React, useEffect, useState } from 'react';
 import { PropTypes } from 'prop-types';
-import { Modal, Button, Badge } from 'react-bootstrap';
+import { Modal, Button, Badge, Alert, CloseButton } from 'react-bootstrap';
 import { BsX } from 'react-icons/bs';
 import { TLPBackend } from '../../common/utils';
 
@@ -11,10 +11,13 @@ import styles from './CreateStudentModal.module.css';
 
 const CreateStudentModal = ({ siteId, teacherId, isOpen, setIsOpen }) => {
   const [error, setError] = useState(null);
-  const [studentFirstName, setStudentFirstName] = useState('First Name');
-  const [studentLastName, setStudentLastName] = useState('Last Name');
+  const [showAlert, setShowAlert] = useState(false);
+  const [formSubmitted, setFormSubmmited] = useState(false);
 
-  const [homeTeacher, setHomeTeacher] = useState('Home Teacher');
+  const [studentFirstName, setStudentFirstName] = useState('');
+  const [studentLastName, setStudentLastName] = useState('');
+
+  const [homeTeacher, setHomeTeacher] = useState('');
 
   const ethnicities = [
     'white',
@@ -86,6 +89,17 @@ const CreateStudentModal = ({ siteId, teacherId, isOpen, setIsOpen }) => {
   }, []);
 
   const createStudent = async () => {
+    if (
+      studentFirstName === '' ||
+      studentLastName === '' ||
+      studentGender === 'Select Gender' ||
+      studentGrade === 'Select Grade' ||
+      studentGroup === 'Select Group'
+    ) {
+      setFormSubmmited(true);
+      return;
+    }
+
     const formData = {
       firstName: studentFirstName,
       lastName: studentLastName,
@@ -100,107 +114,131 @@ const CreateStudentModal = ({ siteId, teacherId, isOpen, setIsOpen }) => {
         'Content-Type': 'application/json',
       },
     });
+    setShowAlert(true);
     closeModal();
   };
 
   return (
-    <Modal show={isOpen} onHide={closeModal}>
-      <Modal.Header closeButton>
-        <Modal.Title className="text-center">Create New Student</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <form className={styles['form-body']}>
-          <div className={styles['input-area']}>
-            <p className={styles.required}>First Name</p>
-            <input
-              type="text"
-              className="form-control"
-              name="firstName"
-              placeholder="First Name"
-              onChange={e => setStudentFirstName(e.target.value)}
-              required
-            />
-          </div>
-          <div className={styles['input-area']}>
-            <p className={styles.required}>Last Name</p>
-            <input
-              type="text"
-              className="form-control"
-              name="lastName"
-              placeholder="Last Name"
-              onChange={e => setStudentLastName(e.target.value)}
-              required
-            />
-          </div>
-
-          <div className={styles['input-area']}>
-            <p>Ethnicity</p>
-            <div className={styles['selected-ethnicity']}>
-              {/* buttons to show which ethnicies are already selected and removed when clicked */}
-              {addedEthnicity.map(ethnic => {
-                return (
-                  <Badge key={ethnic} text="dark" className={styles['ethnicity-badge']}>
-                    <BsX cursor="pointer" name={ethnic} onClick={removeEthnicity} /> {ethnic}
-                  </Badge>
-                );
-              })}
+    <>
+      <Modal show={isOpen} onHide={closeModal}>
+        <Modal.Header closeButton>
+          <Modal.Title className={styles.modalTitle}>Create New Student</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form className={styles['form-body']}>
+            <div className={styles['input-area']}>
+              <p className={styles.required}>First Name</p>
+              <input
+                type="text"
+                className={`form-control ${
+                  studentFirstName === '' && formSubmitted ? styles['input-empty'] : ''
+                }`}
+                name="firstName"
+                placeholder="First Name"
+                onChange={e => setStudentFirstName(e.target.value)}
+                required
+              />
             </div>
-            <DropdownMenu choices={ethnicities} current={ethnicity} setFn={addEthnicity} />
-          </div>
-          <div className={styles['input-area']}>
-            <p className={styles.required}>Gender</p>
-            <DropdownMenu
-              choices={studentGenders}
-              current={studentGender}
-              setFn={setStudentGender}
-              outerClass="w-100"
-            />
-          </div>
-          <div className={styles['input-area']}>
-            <p className={styles.required}>Grade</p>
-            <DropdownMenu
-              choices={studentGrades}
-              current={studentGrade}
-              setFn={setStudentGrade}
-              outerClass="w-100"
-            />
-          </div>
-          <div className={styles['input-area']}>
-            <p>Home Teacher</p>
-            <input
-              type="text"
-              className="form-control"
-              name="homeTeacher"
-              placeholder="Home Teacher"
-              onChange={e => setHomeTeacher(e.target.value)}
-              required
-            />
-          </div>
-          <div className={styles['input-area']}>
-            <p>Add To Student Group</p>
-            <DropdownMenu
-              choices={studentGroupNames}
-              current={studentGroup}
-              setFn={setStudentGroup}
-              outerClass="w-100"
-            />
-          </div>
-        </form>
-      </Modal.Body>
-      <Modal.Footer className="border-0">
-        <Button variant="secondary" className={styles['cancel-button']} onClick={closeModal}>
-          Cancel
-        </Button>
-        <Button
-          type="submit"
-          variant="primary"
-          className={styles['save-button']}
-          onClick={() => createStudent()}
-        >
-          Create
-        </Button>
-      </Modal.Footer>
-    </Modal>
+            <div className={styles['input-area']}>
+              <p className={styles.required}>Last Name</p>
+              <input
+                type="text"
+                className={`form-control ${
+                  studentLastName === '' && formSubmitted ? styles['input-empty'] : ''
+                }`}
+                name="lastName"
+                placeholder="Last Name"
+                onChange={e => setStudentLastName(e.target.value)}
+                required
+              />
+            </div>
+
+            <div className={styles['input-area']}>
+              <p>Ethnicity</p>
+              <div className={styles['selected-ethnicity']}>
+                {/* buttons to show which ethnicies are already selected and removed when clicked */}
+                {addedEthnicity.map(ethnic => {
+                  return (
+                    <Badge key={ethnic} text="dark" className={styles['ethnicity-badge']}>
+                      <BsX cursor="pointer" name={ethnic} onClick={removeEthnicity} /> {ethnic}
+                    </Badge>
+                  );
+                })}
+              </div>
+              <DropdownMenu choices={ethnicities} current={ethnicity} setFn={addEthnicity} />
+            </div>
+            <div className={styles['input-area']}>
+              <p className={styles.required}>Gender</p>
+              <DropdownMenu
+                choices={studentGenders}
+                current={studentGender}
+                setFn={setStudentGender}
+                outerClass="w-100"
+                innerClass={
+                  studentGender === 'Select Gender' && formSubmitted ? styles['input-empty'] : ''
+                }
+              />
+            </div>
+            <div className={styles['input-area']}>
+              <p className={styles.required}>Grade</p>
+              <DropdownMenu
+                choices={studentGrades}
+                current={studentGrade}
+                setFn={setStudentGrade}
+                outerClass="w-100"
+                innerClass={
+                  studentGrade === 'Select Grade' && formSubmitted ? styles['input-empty'] : ''
+                }
+              />
+            </div>
+            <div className={styles['input-area']}>
+              <p>Home Teacher</p>
+              <input
+                type="text"
+                className="form-control"
+                name="homeTeacher"
+                placeholder="Home Teacher"
+                onChange={e => setHomeTeacher(e.target.value)}
+                required
+              />
+            </div>
+            <div className={styles['input-area']}>
+              <p className={styles.required}>Add To Student Group</p>
+              <DropdownMenu
+                choices={studentGroupNames}
+                current={studentGroup}
+                setFn={setStudentGroup}
+                outerClass="w-100"
+                innerClass={
+                  studentGroup === 'Select Group' && formSubmitted ? styles['input-empty'] : ''
+                }
+              />
+            </div>
+          </form>
+        </Modal.Body>
+        <Modal.Footer className="border-0">
+          <Button variant="secondary" className={styles['cancel-button']} onClick={closeModal}>
+            Cancel
+          </Button>
+          <Button
+            type="submit"
+            variant="primary"
+            className={styles['save-button']}
+            onClick={() => createStudent()}
+          >
+            Create
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      {showAlert ? (
+        <div className="center-block">
+          <Alert variant="success" className={styles['alert-custom']}>
+            Student Created Successfully
+            <CloseButton className="alert-close-btn" onClick={() => setShowAlert(false)} />
+          </Alert>
+        </div>
+      ) : null}
+    </>
   );
 };
 
