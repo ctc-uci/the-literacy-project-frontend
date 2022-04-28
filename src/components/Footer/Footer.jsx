799c1eea312322daa7a9ef50058cf6ddd8b820d5
ctc-devops <90984711+ctc-devops@users.noreply.github.com>
2022-04-28 03:45:43 -0500

    aac-footer-updates (#121)
    * Create PR for #120
    
    * update create site placeholders & inputs
    
    * fix heroku error
    
    * fix form control needed error
    
    * updated create site
    
    * add duplicate area name validation
    
    * create footer component, to do add to MT pages
    
    * add firebase file
    
    * Delete firebase-adminsdk.json
    
    * finished create site update
    
    * added footer to all MT pages
    
    * button text color, yup validation fix
    
    * todo
    
    Co-authored-by: allenxluo <allenxluo@users.noreply.github.com>
    Co-authored-by: Allen Luo <allen@wsip-174-77-42-57.oc.oc.cox.net>
    Co-authored-by: allenxluo <40844397+allenxluo@users.noreply.github.com>
    Co-authored-by: anikait <anikaitr@uci.edu>

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import styles from './Footer.module.css';
+import logo from './logo.png';
+
+const Footer = () => {
+  const goTopOfPage = () => {
+    window.scrollTo(0, 0);
+  };
+
+  return (
+    <div>
+      <Row>
+        <div className={styles['button-wrapper']}>
+          <button type="button" className={styles.button} onClick={goTopOfPage}>
+            <div className={styles['to-top']}>↑ Back to Top</div>
+          </button>
+        </div>
+      </Row>
+      <Row className={styles.middle}>
+        <Col md="auto">
+          <div className={styles.wrapper}>
+            <img src={logo} alt={logo} className={styles.logo} />
+          </div>
+        </Col>
+        <Col sm={4}>
+          <div>
+            <a
+              href="https://literacyproj.org"
+              target="_blank"
+              rel="noreferrer"
+              className={styles['footer-link']}
+            >
+              https://literacyproj.org
+            </a>
+          </div>
+          <div>124 Tustin Avenue, Suite 103 | Newport Beach, CA 92663</div>
+        </Col>
+        <Col md="auto">
+          {/* TODO: HELP DOCUMENTATION LINK */}
+          <div>
+            Help Documentation |{' '}
+            <a
+              href="https://literacyproj.org/contact-us"
+              target="_blank"
+              rel="noreferrer"
+              className={styles['footer-link']}
+            >
+              Contact Administration
+            </a>
+          </div>
+          <div>
+            TEL 949.721.1319 |{' '}
+            <a
+              href="https://literacyproj.org/privacy-policy"
+              target="_blank"
+              rel="noreferrer"
+              className={styles['footer-link']}
+            >
+              Privacy Policy
+            </a>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <div className={styles.copyright}>
+          Copyright © 2017, The Literacy Project. All Rights Reserved.
+        </div>
+      </Row>
+    </div>
+  );
+};
+
+export default Footer;
