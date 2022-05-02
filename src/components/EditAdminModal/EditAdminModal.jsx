a6d6554a4686248b925c4225c38494c95b955d7b
ctc-devops <90984711+ctc-devops@users.noreply.github.com>
2022-05-02 16:58:36 -0500

    [Admin] Create/Edit Admin Modal Updates (#132)
    * Create PR for #130
    
    * Start yup and react form integration on Create Admin Modal
    
    * Continue integrating yup/react form into Create Admin Modal
    
    * updated modal styling
    
    * Finish converting Create Admin modal to react form with error checking & restore email link functionality
    
    * updating edit admin modal
    
    * Fix error messages on create admin modal fields messing up field spacing
    
    * Polish functionality for Edit Admin modal, add error messages
    
    * Fix bug with adminData not showing on Edit Admin Modal on load
    
    * Fix EAM status dropdown working properly on active/inactive and add undo changes functionality
    
    * remove console log
    
    Co-authored-by: alanchangxyz <alanchangxyz@users.noreply.github.com>
    Co-authored-by: Alan Chang <alan@alanchang.xyz>
    Co-authored-by: Sravan Tallapaka <sravtall@gmail.com>
    Co-authored-by: anikait <anikaitr@uci.edu>

--- src/components/EditAdminModal/EditAdminModal.jsx
+++ src/components/EditAdminModal/EditAdminModal.jsx
@@ -1,63 +1,138 @@
 import { React, useState, useEffect } from 'react';
 import { PropTypes } from 'prop-types';
-import { Modal, Button, Alert, CloseButton, Form } from 'react-bootstrap';
+import { Modal, Button, Alert, CloseButton } from 'react-bootstrap';
+
+// Forms
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+import styles from './EditAdminModal.module.css';
 import { TLPBackend, reloadPage } from '../../common/utils';
 import WarningModal from '../WarningModal/WarningModal';
 
 const EditAdminModal = ({ isOpen, setIsOpen, adminId }) => {
   const [showEditAdminAlert, setShowEditAdminAlert] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
   const [error, setError] = useState(null);
-  const [email, setEmail] = useState('');
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
+
+  // This admin data will be populated, but may be changed using the form
+  const [adminData, setAdminData] = useState({
+    firstName: '',
+    lastName: '',
+    phoneNumber: '',
+    email: '',
+  });
+  // Copy of initial admin data for undo functionality
+  const [initialAdminData, setInitialAdminData] = useState({
+    firstName: '',
+    lastName: '',
+    phoneNumber: '',
+    email: '',
+    active: '',
+  });
   const [status, setStatus] = useState('');
   const [warningOpen, setWarningOpen] = useState(false);
-  const adminName = `${firstName} ${lastName}`;
+  const adminName = `${adminData.firstName} ${adminData.lastName}`;
 
   const closeModal = () => {
     setIsOpen(false);
     setShowEditAdminAlert(true);
   };
 
-  const updateAdminData = async () => {
-    await TLPBackend.put(`/admins/${adminId}`, {
-      firstName,
-      lastName,
-      phoneNumber,
-      email,
-      active: status.toLowerCase(),
-    });
-    reloadPage();
-    closeModal();
+  const closeModalNoAlert = () => {
+    setIsOpen(false);
+    setShowEditAdminAlert(false);
+    setErrorMessage('');
   };
 
   const openWarningModal = () => {
     setWarningOpen(!warningOpen);
   };
 
+  const undoChanges = async () => {
+    try {
+      const { firstName, lastName, phoneNumber, email, active } = initialAdminData;
+      await TLPBackend.put(`/admins/${adminId}`, {
+        firstName,
+        lastName,
+        phoneNumber,
+        email,
+        active,
+      });
+      setAdminData({ firstName, lastName, email, phoneNumber });
+      setStatus(active);
+      setErrorMessage('');
+      setShowEditAdminAlert(false);
+    } catch (err) {
+      setErrorMessage(err.message);
+    }
+  };
+
+  const schema = yup
+    .object({
+      firstName: yup.string().required('First name is required'),
+      lastName: yup.string().required('Last name is required'),
+      phoneNumber: yup.string().required('Phone number is required'),
+    })
+    .required();
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+    delayError: 750,
+  });
+
+  const onSubmit = async data => {
+    try {
+      const { firstName, lastName, phoneNumber, email } = data;
+      await TLPBackend.put(`/admins/${adminId}`, {
+        firstName,
+        lastName,
+        phoneNumber,
+        email,
+        active: status.toLowerCase(),
+      });
+      setAdminData({ firstName, lastName, email, phoneNumber });
+      setErrorMessage('');
+      closeModal();
+    } catch (err) {
+      setErrorMessage(err.message);
+    }
+  };
+
   const deleteAdmin = async () => {
     // TODO: What to do if it fails ??? Need an error message
-    await TLPBackend.delete(`/admins/${adminId}`);
-    reloadPage();
-    setIsOpen(false);
+    try {
+      await TLPBackend.delete(`/admins/${adminId}`);
+      reloadPage();
+      setIsOpen(false);
+    } catch (err) {
+      setErrorMessage(err.message);
+    }
   };
 
   useEffect(async () => {
     if (!isOpen) return;
-
     try {
       const res = await TLPBackend.get(`/admins/${adminId}`, {
         headers: {
           'Content-Type': 'application/json',
         },
       });
-      const adminData = res.data;
-      setEmail(adminData.email);
-      setFirstName(adminData.firstName);
-      setLastName(adminData.lastName);
-      setPhoneNumber(adminData.phoneNumber);
-      setStatus(adminData.active);
+      if (res.status === 200) {
+        const { firstName, lastName, email, phoneNumber, active } = res.data;
+        setInitialAdminData({ firstName, lastName, email, phoneNumber, active });
+        setAdminData({ firstName, lastName, email, phoneNumber });
+        setStatus(active);
+        reset({ firstName, lastName, email, phoneNumber });
+      } else {
+        setError(error);
+      }
     } catch (err) {
       setError(err);
     }
@@ -65,6 +140,7 @@ const EditAdminModal = ({ isOpen, setIsOpen, adminId }) => {
 
   return (
     <>
+      {/* Warning modal for deleting admin */}
       <WarningModal
         isOpen={warningOpen}
         setIsOpen={setWarningOpen}
@@ -72,61 +148,117 @@ const EditAdminModal = ({ isOpen, setIsOpen, adminId }) => {
         body="admin"
         deleteFunc={deleteAdmin}
       />
-      <Modal show={isOpen} onHide={() => setIsOpen(false)}>
-        <Modal.Header closeButton>
-          <Modal.Title>Edit Admin</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <div>
-            <Form.Group className="mb-5" controlId="editAdminAccount.firstName">
-              <Form.Label>First Name</Form.Label>
-              <Form.Control
-                placeholder="First Name"
-                defaultValue={firstName}
-                onChange={({ target }) => setFirstName(target.value)}
-              />
-            </Form.Group>
-            <Form.Group className="mb-5" controlId="editAdminAccount.lastName">
-              <Form.Label>Last Name</Form.Label>
-              <Form.Control
-                placeholder="Last Name"
-                defaultValue={lastName}
-                onChange={({ target }) => setLastName(target.value)}
-              />
-            </Form.Group>
-            <Form.Group className="mb-5" controlId="editAdminAccount.email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control
+      {/* Actual modal for editing admin */}
+      <Modal
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        show={isOpen}
+        onHide={closeModalNoAlert}
+      >
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Modal.Header closeButton>
+            <Modal.Title className={styles.modalTitle}>Edit Admin</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>
+            <div>
+              <label htmlFor="first-name" className={styles.fNameField}>
+                <h3 className={styles.requiredSubtitles}>First Name</h3>
+                <input
+                  type="text"
+                  name="firstName"
+                  key={adminData.firstName}
+                  defaultValue={adminData.firstName}
+                  className={`form-control ${errors.firstName ? `is-invalid` : ''}`}
+                  {...register('firstName')}
+                />
+                <div className={`text-danger ${styles['err-msg']}`}>
+                  {errors.firstName?.message ?? <>{'\u00A0'}</>}
+                </div>
+              </label>
+              <label htmlFor="last-name" className={styles.lNameField}>
+                <h3 className={styles.requiredSubtitles}>Last Name</h3>
+                <input
+                  type="text"
+                  name="lastName"
+                  key={adminData.lastName}
+                  defaultValue={adminData.lastName}
+                  className={`form-control ${errors.lastName ? `is-invalid` : ''}`}
+                  {...register('lastName')}
+                />
+                <div className={`text-danger ${styles['err-msg']}`}>
+                  {errors.lastName?.message ?? <>{'\u00A0'}</>}
+                </div>
+              </label>
+            </div>
+            <label htmlFor="email" className={styles.emailField}>
+              <h3 className={styles.requiredSubtitles}>Email</h3>
+              <input
+                disabled
                 type="email"
-                placeholder="example@gmail.com"
-                defaultValue={email}
-                onChange={({ target }) => setEmail(target.value)}
+                name="email"
+                placeholder="Email"
+                value={adminData.email}
+                className="form-control"
               />
-            </Form.Group>
-            <Form.Group className="mb-5" controlId="editAdminAccount.status">
-              <Form.Label>Status</Form.Label>
-              <Form.Control as="select" onChange={({ target }) => setStatus(target.value)}>
-                <option value="Active">Active</option>
-                <option value="Inactive">Inactive</option>
-              </Form.Control>
-            </Form.Group>
-          </div>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="danger" onClick={openWarningModal}>
-            Delete
-          </Button>
-          <Button variant="primary" onClick={updateAdminData}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
+            </label>
+            <div>
+              <label htmlFor="phone-number" className={styles.phoneNumField}>
+                <h3 className={styles.requiredSubtitles}>Phone Number</h3>
+                <input
+                  type="text"
+                  name="phoneNumber"
+                  placeholder="Phone Number"
+                  key={adminData.phoneNumber}
+                  defaultValue={adminData.phoneNumber}
+                  className={`form-control ${errors.phoneNumber ? `is-invalid` : ''}`}
+                  {...register('phoneNumber')}
+                />
+              </label>
+              <div className={`text-danger ${styles['err-msg']}`}>
+                {errors.phoneNumber?.message ?? <>{'\u00A0'}</>}
+              </div>
+              {/* {JSON.stringify(errors)} */}
+            </div>
+            <label htmlFor="active" className={styles.emailField}>
+              <h3 className={styles.requiredSubtitles}>Status</h3>
+              <select
+                name="active"
+                className="form-control"
+                onChange={e => setStatus(e.target.value)}
+              >
+                <option selected={status === 'active'} value="Active">
+                  Active
+                </option>
+                <option selected={status === 'inactive'} value="Inactive">
+                  Inactive
+                </option>
+              </select>
+            </label>
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button variant="danger" onClick={openWarningModal}>
+              Delete
+            </Button>
+            <Button variant="primary" type="submit">
+              Save Changes
+            </Button>
+            {errorMessage && <p>{errorMessage}</p>}
+          </Modal.Footer>
+        </form>
       </Modal>
+
       {showEditAdminAlert ? (
         <Alert variant="primary" className="alert-custom">
-          {`Updated ${firstName} ${lastName}'s information.`}{' '}
-          <Alert.Link href="/" className="alert-link-custom">
+          {`Updated ${adminData.firstName} ${adminData.lastName}'s information.`}{' '}
+          <span
+            aria-hidden
+            className={`alert-link-custom underline ${styles.undoChanges}`}
+            onClick={undoChanges}
+          >
             UNDO
-          </Alert.Link>
+          </span>
           <CloseButton className="alert-close-btn" onClick={() => setShowEditAdminAlert(false)} />
         </Alert>
       ) : null}
