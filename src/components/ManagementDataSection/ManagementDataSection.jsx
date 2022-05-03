0645c79f9a655402a10b770c267b68ed4a30fcd3
anikait <anikaitr@uci.edu>
2022-05-02 22:19:37 -0700

    remove old changes

--- src/components/ManagementDataSection/ManagementDataSection.jsx
+++ src/components/ManagementDataSection/ManagementDataSection.jsx
@@ -26,16 +26,9 @@ const ManagementDataSection = ({
     popover = null;
   }
   const [modalIsOpen, setModalOpen] = useState('');
-  const [sortBy, setSortBy] = useState('A-Z');
-  const [searchText, setSearchText] = useState('');
-
-  const sorts = ['A-Z', 'Z-A'];
 
   const clickManager = () => {
     setModalOpen(sectionTitle);
-  };
-  const inputHandler = e => {
-    setSearchText(e.target.value.toLowerCase());
   };
 
   const displaySectionTitle = () => {
@@ -88,51 +81,17 @@ const ManagementDataSection = ({
     return null;
   };
 
-  // Compares two fields alphabetically
-  const compareNames = (field1, field2) => {
-    const f1 = field1.items[0].toLowerCase(); // items[0] gives the Name
-    const f2 = field2.items[0].toLowerCase();
-
-    switch (sortBy) {
-      case 'A-Z':
-        return f1 < f2 ? -1 : 1;
-      case 'Z-A':
-        return f1 < f2 ? 1 : -1;
-      default:
-        return f1 < f2 ? -1 : 1;
-    }
-  };
-
   const displaySortByButton = () => {
     return (
       <DropdownButton
         className={styles['dropdown-button']}
         id="dropdown-basic-button"
-        title={`Sort By: ${sortBy}`}
+        title="Sort By"
       >
-        {sorts.map(sort => (
-          <Dropdown.Item
-            onClick={() => {
-              setSortBy(sort);
-            }}
-            key={sort}
-          >
-            {sort}
-          </Dropdown.Item>
-        ))}
+        <Dropdown.Item>A-Z</Dropdown.Item>
+        <Dropdown.Item>Z-A</Dropdown.Item>
       </DropdownButton>
     );
-  };
-
-  const search = data => {
-    return data.filter(row => {
-      const name = row.items[0].toLowerCase();
-      return name.includes(searchText);
-    });
-  };
-
-  const displayData = data => {
-    return search(data).sort(compareNames);
   };
 
   return (
@@ -142,7 +101,7 @@ const ManagementDataSection = ({
         <div className={styles['inner-ctrl']}>{displayCreateButton()}</div>
         <div className={styles['inner-ctrl']}>
           <div className={styles.search}>
-            <InputGroup input={searchText} onChange={inputHandler}>
+            <InputGroup>
               <FormControl
                 className={styles['search-bar']}
                 placeholder="Search"
@@ -160,7 +119,7 @@ const ManagementDataSection = ({
       </div>
       <Table
         theadData={theadData}
-        tbodyData={displayData(tbodyData)}
+        tbodyData={tbodyData}
         tbodyColIsBadge={tbodyColIsBadge}
         sectionTitle={sectionTitle}
         statusCol={statusCol}
